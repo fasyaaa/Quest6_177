4bdca25a30001f4f97e7a3f2d1f9d18a3803a839
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-11-21 22:29:58 +0700

    membuat function PengelolaHalaman

--- app/src/main/java/com/example/mvvmpt2/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/mvvmpt2/navigation/PengelolaHalaman.kt
@@ -1,2 +1,17 @@
 package com.example.mvvmpt2.navigation
 
+import androidx.compose.runtime.Composable
+
+enum class halaman{
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+
+@Composable
+fun PengelolaHalaman(
+
+){
+
+}
