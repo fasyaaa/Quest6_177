d6788f0454905c1583029b3620bc45ed0de87818
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-11-21 21:44:28 +0700

    menyelasaikan val option

--- app/src/main/java/com/example/mvvmpt2/data/MataKuliah.kt
+++ app/src/main/java/com/example/mvvmpt2/data/MataKuliah.kt
@@ -2,6 +2,11 @@ package com.example.mvvmpt2.data
 
 object MataKuliah {
     val option = listOf(
-        
+        "Pemrograman Multiplatform",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
     )
 }
