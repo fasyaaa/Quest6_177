3d65bd17646d2b58e1c31bc701dbf6be8b2bffb0
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-11-21 22:27:43 +0700

    menyelesaikan bagian RencanaStudyViewModel.kt

--- app/src/main/java/com/example/mvvmpt2/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/mvvmpt2/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.mvvmpt2.ui.view.viewmodel
 
+import com.example.mvvmpt2.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
 class RencanaStudyViewModel {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String){
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            mataKuliah = ls[0],
+            kelas = ls[1]
+        ) }
+    }
 }
