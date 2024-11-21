d38aff377b1065e5430a3b49de59a05adcf8c934
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-11-21 22:18:31 +0700

    menambahkan check box

--- app/src/main/java/com/example/mvvmpt2/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/mvvmpt2/ui/view/screen/RencanaStudiView.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
@@ -149,7 +150,11 @@ fun RencanaStudiView(
                 Spacer(modifier = Modifier.padding(8.dp))
                 Text(text = "Klausul Persetujuan Mahasiswa", fontWeight = FontWeight.Bold)
                 Row(verticalAlignment = Alignment.CenterVertically){
-
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = { checked = it},
+                        enabled = chosenDropDown.isNotBlank() && pilihanKelas.isNotBlank()
+                    )
                 }
             }
         }
