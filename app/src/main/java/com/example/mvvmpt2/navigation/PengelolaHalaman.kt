4c0267cf73af87d81ef5d86741863eaf23f1b1c6
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-11-21 22:51:34 +0700

    menambahkan navigasi untuk slide 1

--- app/src/main/java/com/example/mvvmpt2/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/mvvmpt2/navigation/PengelolaHalaman.kt
@@ -1,11 +1,16 @@
 package com.example.mvvmpt2.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.mvvmpt2.ui.view.screen.SplashView
 import com.example.mvvmpt2.ui.view.viewmodel.MahasiswaViewModel
 import com.example.mvvmpt2.ui.view.viewmodel.RencanaStudyViewModel
 
@@ -23,7 +28,17 @@ fun PengelolaHalaman(
     mahasiswaViewModel: MahasiswaViewModel = viewModel(),
     krsViewModel: RencanaStudyViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
-
 ){
-
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = halaman.Splash.name,
+        modifier = Modifier.padding()
+    ){
+        composable(route = halaman.Splash.name){
+            SplashView (onMulaiButton =  {
+                navController.navigate(halaman.Mahasiswa.name)
+            })
+        }
+    }
 }
